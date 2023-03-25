090eb815ad0d02f81900fba2f95927269ea531a4
Riccardo Silvi <silvicardo@gmail.com>
2023-03-25 11:46:20 +0100

    feat(manager/balance): calculate full fiat deposits (#8)
    * feat(db): export all selectors from index file
    
    * feat(db/selectors): young fiat deposits selector
    
    * feat(manager/balance): getFiatDepositOperationsTotal calculate function

--- src/db/selectors/index.ts
+++ src/db/selectors/index.ts
@@ -1,7 +1,15 @@
 import { selectors as youngPlatformSelectors } from "./YOUNG_PLATFORM";
 import { selectors as cryptoComExchangeSelectors } from "./CRYPTO_COM_EXCHANGE";
+import { selectors as cryptoComAppSelectors } from "./CRYPTO_COM_APP";
+import { selectors as bitpandaSelectors } from "./BITPANDA";
+import { selectors as bitpandaProSelectors } from "./BITPANDA_PRO";
+import { selectors as nexoSelectors } from "./NEXO";
 
 export const selectors = {
   youngPlatform: youngPlatformSelectors,
   cryptoComExchange: cryptoComExchangeSelectors,
+  cryptoComApp: cryptoComAppSelectors,
+  bitpanda: bitpandaSelectors,
+  bitpandaPro: bitpandaProSelectors,
+  nexo: nexoSelectors,
 };

de3d36ac11c9fd91460186e30e837d3628a51ba8
silvicardo <silvicardo@gmail.com>
2023-05-13 17:57:29 +0200

    feat(db/selectors): crypto com app trades selector

--- src/db/selectors/CRYPTO_COM_APP/index.ts
+++ src/db/selectors/CRYPTO_COM_APP/index.ts
@@ -1,6 +1,9 @@
 import * as depositsSelectors from "./deposits";
 import * as withdrawalsSelectors from "./withdrawal";
+
+import * as tradeSelectors from "./trade";
 export const selectors = {
   deposits: depositsSelectors,
   withdrawals: withdrawalsSelectors,
+  trade: tradeSelectors,
 };
---
+++ src/db/selectors/CRYPTO_COM_APP/trade.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "@prisma/client";
+import { trim } from "lodash";
+import { PairQueryInput, TradeQueryConfig } from "../types";
+import { queryUtils } from "../utils";
+
+/*
+ * Queries for both fiat <-> crypto
+ * and crypto <-> crypto trades
+ * buy crypto with fiat can be
+ * - crypto_purchase ( buy with debit card )
+ * - viban_purchase ( buy with fiat wallet balance )
+ * sell crypto for fiat can be
+ * - crypto_viban_exchange ( sell to fiat wallet balance )
+ * crypto to crypto can be
+ * - crypto_exchange
+ */
+export const getForPair = (prisma: PrismaClient, config: TradeQueryConfig) => {
+  const { pair, side, timestamp } = config;
+  const [base, quote] = trim(pair).split("_") as [
+    PairQueryInput,
+    PairQueryInput
+  ];
+  let assetParams = {};
+  if (base !== "*" && quote !== "*") {
+    assetParams = {
+      transactionDescription:
+        side === "buy" ? `${quote} -> ${base}` : `${base} -> ${quote}`,
+      currency: side === "buy" ? quote : base,
+      toCurrency: side === "buy" ? base : quote,
+    };
+  }
+
+  if (base === "*" && quote !== "*") {
+    assetParams = {
+      ...assetParams,
+      [side === "buy" ? "currency" : "toCurrency"]: quote,
+    };
+  }
+
+  if (quote === "*" && base !== "*") {
+    assetParams = {
+      ...assetParams,
+      [side === "buy" ? "toCurrency" : "currency"]: base,
+    };
+  }
+  const timestampQueryParam = !timestamp
+    ? {}
+    : { timestampUtc: queryUtils.getTimespanQueryObject(timestamp) };
+
+  return prisma.cryptoComCryptoTransaction.findMany({
+    where: {
+      OR: [
+        // fiat (wallet balance ) <-> crypto
+        {
+          transactionKind: {
+            in:
+              side === "buy"
+                ? ["crypto_exchange", "viban_purchase"]
+                : ["crypto_exchange", "crypto_viban_exchange"],
+          },
+          ...assetParams,
+          ...timestampQueryParam,
+        },
+        // buy crypto with fiat debit card
+        side === "buy" && ["*", "EUR"].includes(quote)
+          ? {
+              transactionKind: "crypto_purchase",
+              ...(base !== "*"
+                ? {
+                    transactionDescription: `Buy ${base}`,
+                    currency: base,
+                  }
+                : {
+                    transactionDescription: {
+                      contains: "Buy",
+                    },
+                  }),
+              nativeCurrency: "EUR",
+              ...timestampQueryParam,
+            }
+          : {},
+      ],
+    },
+  });
+};
+
+/*
+getForPair(new PrismaClient(), {
+  pair: "BTC_*",
+  side: "buy",
+}).then((r) => console.log(r, r.length));
+
+ */

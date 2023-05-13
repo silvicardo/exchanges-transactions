358c935b0dd04c3dccdcc6f4c487ff1476680098
silvicardo <silvicardo@gmail.com>
2023-05-13 12:34:36 +0200

    feat(manager): add young platform to sell to fiat calculation

--- src/manager/trade/sell.ts
+++ src/manager/trade/sell.ts
@@ -2,22 +2,24 @@ import { CurrencyName, PrismaClient } from "@prisma/client";
 import { database } from "../../db";
 import { QueryTimespan } from "../../db/selectors/utils";
 
-export const getSellToFiatOperations = async ({
-  crypto,
-  timestamp,
-}: {
+type SellToFiatConfig = {
   crypto: Exclude<CurrencyName, "EUR" | "USD" | "GBP"> | "*";
   timestamp?: Partial<QueryTimespan>;
-}): Promise<{
+};
+export const getSellToFiatOperations = async (
+  config: SellToFiatConfig
+): Promise<{
   account: Record<string, number>;
   total: number;
+  config: SellToFiatConfig;
 }> => {
+  const { crypto, timestamp } = config;
   const prisma = new PrismaClient();
   const { selectors } = database;
 
   const bitpanda = (
     await selectors.bitpanda.trade.getForFiatPair(prisma, {
-      crypto: "*",
+      crypto: crypto,
       side: "sell",
       timestamp,
     })
@@ -25,22 +27,36 @@ export const getSellToFiatOperations = async ({
 
   const bitpandaPro = (
     await selectors.bitpandaPro.trade.getForPair(prisma, {
-      pair: "*_EUR",
+      pair: `${crypto}_EUR`,
       side: "sell",
       timestamp,
     })
   ).reduce((acc, curr) => acc + curr.price, 0);
 
+  const youngPlatform = (
+    await selectors.youngPlatform.sell.getForPair(prisma, {
+      pair: `${crypto}_EUR`,
+      timestamp,
+    })
+  ).reduce((acc, curr) => acc + curr.amount, 0);
+
   return {
+    config,
     account: {
       bitpanda,
       bitpandaPro,
+      youngPlatform,
     },
-    total: bitpanda + bitpandaPro,
+    total: bitpanda + bitpandaPro + youngPlatform,
   };
 };
-
-getSellToFiatOperations({
-  crypto: "*",
-  timestamp: { gte: "2021-01-01", lte: "2022-12-31" },
-}).then(console.log);
+// ts-node src/manager/trade/sell.ts
+const queryCurrencies = ["*", "BTC", "ETH", "USDT", "USDC"] as const;
+Promise.all(
+  queryCurrencies.map((crypto) =>
+    getSellToFiatOperations({
+      crypto,
+      timestamp: { gte: "2022-01-01", lte: "2022-12-31" },
+    })
+  )
+).then((r) => r.forEach((d) => console.log(d.config.crypto, `${d.total} €`)));

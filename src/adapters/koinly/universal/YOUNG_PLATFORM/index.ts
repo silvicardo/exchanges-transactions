e72dc55e14763222ccc9848ec3d5540dc266a05d
Riccardo Silvi <silvicardo@gmail.com>
2023-05-13 10:16:28 +0200

    feat: add yp admin gift staking movement handler and selectors (#14)

--- src/adapters/koinly/universal/YOUNG_PLATFORM/index.ts
+++ src/adapters/koinly/universal/YOUNG_PLATFORM/index.ts
@@ -1,5 +1,8 @@
 import { handle as handleWithdrawals } from "./withdrawals";
 import { handle as handleDeposits } from "./deposits";
+import { handle as handleStaking } from "./staking";
+import { handle as handleAdmin } from "./admin";
+import { handle as handleGift } from "./gift";
 import { handle as handleTradeBuy } from "./trade_buy";
 import { handle as handleTradeSell } from "./trade_sell";
 import { PrismaClient } from "@prisma/client";
@@ -17,15 +20,26 @@ export const handle = async () => {
 
   const dbSellTrades = await selectors.sell.getAll(prisma);
 
+  const dbGiftsMovements = await selectors.gift.getAll(prisma);
+  const dbStakingMovements = await selectors.staking.getAll(prisma);
+
+  const dbAdminMovements = await selectors.admin.getAll(prisma);
+
   const entries = [
     ...handleWithdrawals(dbWithdrawals),
     ...handleDeposits(dbDeposits),
+    ...handleStaking(dbStakingMovements),
+    ...handleAdmin(dbAdminMovements),
+    ...handleGift(dbGiftsMovements),
     ...dbBuyTrades.map(handleTradeBuy),
     ...dbSellTrades.map(handleTradeSell),
   ];
   const sorted = orderBy(entries, [(obj) => new Date(obj.Date)], ["asc"]);
 
-  return convertJSONtoCSV(sorted, "koinly_universal.csv");
+  return convertJSONtoCSV(
+    sorted,
+    "koinly_universal_with_staking_admin_gift.csv"
+  );
 };
 
 handle();
--- src/db/selectors/YOUNG_PLATFORM/index.ts
+++ src/db/selectors/YOUNG_PLATFORM/index.ts
@@ -2,9 +2,15 @@ import * as depositsSelectors from "./deposits";
 import * as withdrawalsSelectors from "./withdrawals";
 import * as buySelectors from "./buy";
 import * as sellSelectors from "./sell";
+import * as stakingSelectors from "./staking";
+import * as adminSelectors from "./admin";
+import * as giftSelectors from "./gift";
 export const selectors = {
   deposits: depositsSelectors,
   withdrawals: withdrawalsSelectors,
   buy: buySelectors,
   sell: sellSelectors,
+  staking: stakingSelectors,
+  admin: adminSelectors,
+  gift: giftSelectors,
 };

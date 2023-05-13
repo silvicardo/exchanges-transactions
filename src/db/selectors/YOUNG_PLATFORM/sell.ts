69e6b975c09198662f501a176717a60fdf30eb8d
silvicardo <silvicardo@gmail.com>
2023-05-13 12:33:24 +0200

    feat(db/selectors): young platform sell getForPair selector

--- src/db/selectors/YOUNG_PLATFORM/sell.ts
+++ src/db/selectors/YOUNG_PLATFORM/sell.ts
@@ -3,10 +3,33 @@ import {
   PrismaPromise,
   YoungPlatformTrade,
 } from "@prisma/client";
+import { PairQueryInput, TradeQueryConfig } from "../types";
+import { queryUtils } from "../utils";
 export const getAll = (
   prisma: PrismaClient
 ): PrismaPromise<YoungPlatformTrade[]> => {
   return prisma.youngPlatformTrade.findMany({
     where: { side: "SELL" },
+  });
+};
+export const getForPair = (
+  prisma: PrismaClient,
+  config: Omit<TradeQueryConfig, "side">
+): PrismaPromise<YoungPlatformTrade[]> => {
+  const { pair, timestamp } = config;
+  const [base, quote] = pair.split("_") as [PairQueryInput, PairQueryInput];
+  const assetParams = {
+    ...(base === "*" ? {} : { base }),
+    ...(quote === "*" ? {} : { quote }),
+  };
+  const timestampQueryParam = !timestamp
+    ? {}
+    : { date: queryUtils.getTimespanQueryObject(timestamp) };
+  return prisma.youngPlatformTrade.findMany({
+    where: {
+      side: "SELL",
+      ...assetParams,
+      ...timestampQueryParam,
+    },
   });
 };

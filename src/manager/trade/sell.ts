66fd122a983665248b3612510492022edb41e082
silvicardo <silvicardo@gmail.com>
2023-05-13 11:34:09 +0200

    feat(manager): add bitpanda pro to getSellToFiatOperations calculation function

--- src/manager/trade/sell.ts
+++ src/manager/trade/sell.ts
@@ -13,24 +13,34 @@ export const getSellToFiatOperations = async ({
   total: number;
 }> => {
   const prisma = new PrismaClient();
+  const { selectors } = database;
 
   const bitpanda = (
-    await database.selectors.bitpanda.trade.getForFiatPair(prisma, {
+    await selectors.bitpanda.trade.getForFiatPair(prisma, {
       crypto: "*",
       side: "sell",
       timestamp,
     })
   ).reduce((acc, curr) => acc + curr.amountFiat, 0);
 
+  const bitpandaPro = (
+    await selectors.bitpandaPro.trade.getForPair(prisma, {
+      pair: "*_EUR",
+      side: "sell",
+      timestamp,
+    })
+  ).reduce((acc, curr) => acc + curr.price, 0);
+
   return {
     account: {
       bitpanda,
+      bitpandaPro,
     },
-    total: bitpanda,
+    total: bitpanda + bitpandaPro,
   };
 };
 
 getSellToFiatOperations({
   crypto: "*",
-  timestamp: { gte: "2022-01-01", lte: "2022-12-31" },
+  timestamp: { gte: "2021-01-01", lte: "2022-12-31" },
 }).then(console.log);

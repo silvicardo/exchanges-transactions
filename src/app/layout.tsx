4139dae8840f991af9d16cddbf2fcb90c3e613d8
Riccardo Silvi <silvicardo@gmail.com>
2023-06-18 15:44:10 +0200

    chore: setup next js for frontend (#29)
    * chore: setup pnpm, next js, prisma
    
    * feat: base yp deposits query test component
    
    * chore: setup chakra

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Providers } from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Crypto Transactions Manager",
+  description: "In House tool to store and manage crypto transactions",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}

b290fcf73bae73170f14ebea8b42660fc55b8c4e
silvicardo <silvicardo@gmail.com>
2023-08-14 17:10:37 +0200

    feat(components): data table component

---
+++ src/components/data-table/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React from "react";
+import {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { isDate } from "lodash";
+
+type BaseDataEntry = {
+  id: number;
+};
+
+export default function DataTable<K extends BaseDataEntry = BaseDataEntry>({
+  data,
+  caption,
+}: {
+  caption: string;
+  data: K[];
+}) {
+  const getRow = React.useCallback((row: K[][number]) => {
+    return (
+      <Tr key={row.id as number}>
+        {Object.entries(row).map(([name, value]) => {
+          if (value === null) {
+            return (
+              <Td key={`${row.id}-${name}-null`} textAlign={"center"}>
+                -
+              </Td>
+            );
+          }
+          if (!name.toLowerCase().includes("id") && typeof value === "number") {
+            return (
+              <Td key={`${row.id}-${name}`} isNumeric>
+                {value.toString()}
+              </Td>
+            );
+          }
+          if (isDate(value)) {
+            return <Td key={`${row.id}-${name}`}>{value.toLocaleString()}</Td>;
+          }
+          return <Td key={`${row.id}-${name}`}>{value.toString()}</Td>;
+        })}
+      </Tr>
+    );
+  }, []);
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>{caption}</TableCaption>
+        <Thead>
+          <Tr>
+            {Object.keys(data[0]).map((key) => (
+              <Th key={key}>{key}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>{data.map(getRow)}</Tbody>
+      </Table>
+    </TableContainer>
+  );
+}

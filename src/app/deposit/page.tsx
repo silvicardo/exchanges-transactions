23f2f19c9183f29838163ab4d42fc8194aca9ab8
silvicardo <silvicardo@gmail.com>
2023-08-14 15:43:17 +0200

    refactor(app/deposit): remove redundant page container

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Heading, Container, VStack } from "@/src/components/chakra";
+import { Heading, VStack } from "@/src/components/chakra";
 import Fiat from "@/src/app/deposit/fiat";
 import { getLastYearTimestamp } from "@/src/utils/date";
 export default async function Deposit() {
   return (
-    <Container maxW={"container.xl"}>
-      <VStack align={"flex-start"} spacing={4}>
-        <Heading>Deposits</Heading>
-        <Fiat timestamp={getLastYearTimestamp()} />
-      </VStack>
-    </Container>
+    <VStack align={"flex-start"} spacing={4}>
+      <Heading>Deposits</Heading>
+      <Fiat timestamp={getLastYearTimestamp()} />
+    </VStack>
   );
 }

5cccae2a62983c4c481d37956e1a0b610102c95c
Riccardo Silvi <silvicardo@gmail.com>
2023-08-16 15:06:11 +0200

    feat(app): nexo borrow (#37)
    * feat(db/selectors): nexo borrow selectors
    
    * feat(app/home): borrow data, display all total data for each year from 2021

--- src/db/selectors/NEXO/index.ts
+++ src/db/selectors/NEXO/index.ts
@@ -1,6 +1,8 @@
 import * as depositsSelectors from "./deposits";
 import * as tradeSelectors from "./trade";
+import * as borrowSelectors from "./borrow";
 export const selectors = {
   deposits: depositsSelectors,
   trade: tradeSelectors,
+  borrow: borrowSelectors,
 };

b97cf0e98583d146fa2be51e5f95ece1c94465a4
silvicardo <silvicardo@gmail.com>
2023-08-17 20:14:31 +0200

    feat(db): export handlers from index object

---
+++ src/db/handlers/index.ts
@@ -0,0 +1,13 @@
+import { handlers as youngPlatform } from "./YOUNG_PLATFORM";
+import { handlers as cryptoComApp } from "./CRYPTO_COM_APP";
+import { handlers as bitpandaPro } from "./BITPANDA_PRO";
+import { handlers as bitpanda } from "./BITPANDA";
+import { handlers as nexo } from "./NEXO";
+
+export const handlers = {
+  youngPlatform,
+  cryptoComApp,
+  bitpandaPro,
+  bitpanda,
+  nexo,
+};

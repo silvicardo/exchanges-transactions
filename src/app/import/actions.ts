82bb9909c7d399fb5fc3c3fcefffea6827e7a5ef
silvicardo <silvicardo@gmail.com>
2023-08-18 19:53:09 +0200

    feat(app/import): data handling after file import

--- src/app/import/actions.ts
+++ src/app/import/actions.ts
@@ -5,7 +5,9 @@ import { writeFile } from "fs/promises";
 import { join } from "path";
 import { ImportFieldValues } from "@/src/app/import/form-options";
 import fs from "fs";
-
+import { camelCase } from "lodash";
+import { database } from "@/src/db";
+const FAKE_USER_ACCOUNT_ID = 1234;
 export const importExchangeData = async (exchangeData: FormData) => {
   const validationResult = validationSchema.safeParse(exchangeData);
 
@@ -15,7 +17,9 @@ export const importExchangeData = async (exchangeData: FormData) => {
       error: validationResult.error.errors[0].message ?? "validation failed",
     };
   }
-  const file = validationResult.data.file as unknown as File;
+  const { year, filename, exchange, file } =
+    validationResult.data as ImportFieldValues;
+
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
@@ -24,11 +28,11 @@ export const importExchangeData = async (exchangeData: FormData) => {
     "/",
     "csvs",
     "/",
-    validationResult.data.year,
+    year,
     "/",
-    validationResult.data.exchange,
+    exchange,
     "/",
-    `${validationResult.data.filename}.csv`
+    `${filename}.csv`
   );
   try {
     await writeFile(path, buffer);
@@ -36,6 +40,85 @@ export const importExchangeData = async (exchangeData: FormData) => {
     return {
       success: false,
       error: `unable write file to path: ${path}`,
+    };
+  }
+
+  const handlerFolder = camelCase(exchange) as keyof typeof database.handlers;
+
+  if (!database.handlers.hasOwnProperty(handlerFolder)) {
+    return {
+      success: false,
+      error: "file written but no handler found for that exchange",
+    };
+  }
+  if (!database.handlers[handlerFolder].hasOwnProperty(filename)) {
+    return {
+      success: false,
+      error: "file written but no handler found for that exchange",
+    };
+  }
+  try {
+    switch (exchange) {
+      case "YOUNG_PLATFORM":
+        if (database.handlers.youngPlatform.hasOwnProperty(filename)) {
+          await database.handlers.youngPlatform[
+            filename as keyof typeof database.handlers.youngPlatform
+          ]({
+            year,
+            userAccountId: FAKE_USER_ACCOUNT_ID,
+          });
+        }
+        break;
+      case "BITPANDA":
+        if (database.handlers.bitpandaPro.hasOwnProperty(filename)) {
+          await database.handlers.bitpandaPro[
+            filename as keyof typeof database.handlers.bitpandaPro
+          ]({
+            year,
+            userAccountId: FAKE_USER_ACCOUNT_ID,
+          });
+        }
+        break;
+      case "BITPANDA_PRO":
+        if (database.handlers.bitpandaPro.hasOwnProperty(filename)) {
+          await database.handlers.bitpandaPro[
+            filename as keyof typeof database.handlers.bitpandaPro
+          ]({
+            year,
+            userAccountId: FAKE_USER_ACCOUNT_ID,
+          });
+        }
+        break;
+      case "NEXO":
+        if (database.handlers.nexo.hasOwnProperty(filename)) {
+          await database.handlers.nexo[
+            filename as keyof typeof database.handlers.nexo
+          ]({
+            year,
+            userAccountId: FAKE_USER_ACCOUNT_ID,
+          });
+        }
+        break;
+      case "CRYPTO_COM_APP":
+        if (database.handlers.cryptoComApp.hasOwnProperty(filename)) {
+          await database.handlers.cryptoComApp[
+            filename as keyof typeof database.handlers.cryptoComApp
+          ]({
+            year,
+            userAccountId: FAKE_USER_ACCOUNT_ID,
+          });
+        }
+        break;
+      default:
+        return {
+          success: false,
+          error: "file written but no handler found for that exchange",
+        };
+    }
+  } catch (e) {
+    return {
+      success: false,
+      error: "file written but handler failed",
     };
   }
 

7358fc00d4de80fdb4485d6d74678e54368bdfb7
silvicardo <silvicardo@gmail.com>
2023-08-22 19:02:14 +0200

    feat(adapters): dust conversions in koinly crypto com exchange adapter

--- src/adapters/koinly/universal/CRYPTO_COM_EXCHANGE/index.ts
+++ src/adapters/koinly/universal/CRYPTO_COM_EXCHANGE/index.ts
@@ -1,28 +1,33 @@
-import { database } from "../../../../db";
+import { database } from "@/src/db";
 import { handle as handleTrade } from "./trades";
 import { handle as handleDeposit } from "./deposits";
 import { handle as handleWithdrawal } from "./withdrawals";
+import { handle as handleDustConversion } from "./dust";
 import { orderBy } from "lodash";
-import { convertJSONtoCSV } from "../../../../../convertJSONtoCSV";
+import { convertJSONtoCSV } from "@/convertJSONtoCSV";
+import { format } from "date-fns";
 
 export const handle = async () => {
   const trades = await database.selectors.cryptoComExchange.trades.getAll();
   const deposits = await database.selectors.cryptoComExchange.deposits.getAll();
   const withdrawals =
     await database.selectors.cryptoComExchange.withdrawals.getAll();
+  const dustConversions = await database.selectors.cryptoComExchange.dust.get();
 
   const entries = [
     ...trades.map(handleTrade),
     ...deposits.map(handleDeposit),
     ...withdrawals.map(handleWithdrawal),
+    ...dustConversions.map(handleDustConversion),
   ];
 
   const sorted = orderBy(entries, [(obj) => new Date(obj.Date)], ["asc"]);
 
   return convertJSONtoCSV(
     sorted,
-    "koinly_universal_crypto_com_exchange_full.csv"
+    `convert/koinly/universal_crypto_com_exchange_${format(
+      new Date(),
+      "yyyy_MM_dd_HH_mm_ss"
+    )}.csv`
   );
 };
-
-handle();

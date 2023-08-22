77f4cfb5c064e2045a02a652ba60b3ea6abe6b2f
silvicardo <silvicardo@gmail.com>
2023-08-22 16:03:33 +0200

    feat(db): export ledger selectors

--- src/db/selectors/index.ts
+++ src/db/selectors/index.ts
@@ -4,6 +4,7 @@ import { selectors as cryptoComAppSelectors } from "./CRYPTO_COM_APP";
 import { selectors as bitpandaSelectors } from "./BITPANDA";
 import { selectors as bitpandaProSelectors } from "./BITPANDA_PRO";
 import { selectors as nexoSelectors } from "./NEXO";
+import { selectors as ledgerSelectors } from "./LEDGER";
 
 export const selectors = {
   youngPlatform: youngPlatformSelectors,
@@ -12,4 +13,5 @@ export const selectors = {
   bitpanda: bitpandaSelectors,
   bitpandaPro: bitpandaProSelectors,
   nexo: nexoSelectors,
+  ledger: ledgerSelectors,
 };
